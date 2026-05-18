f1e401e74e91f006909896009bc9b24e7e4a255a
phuongwj <phamjulia9@gmail.com>
2026-05-18 18:35:26 -0300

    potential fix again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   adapter: cloudflare({
-    imageService: 'passthrough',
+    platformProxy: {
+      enabled: true,
+    },
   }),
   integrations: [react()],
   vite: {
